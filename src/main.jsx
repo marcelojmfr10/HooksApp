54e5501b993543a9b5b132bb49c6dc492766d864
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-03-13 18:55:22 -0600

    useContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { BrowserRouter } from "react-router";
 import { HooksApp } from './HooksApp'
 import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -14,12 +16,15 @@ import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-context/MainApp'
 
 // import './08-useReducer/intro-reducer';
 
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <TodoApp />
-  // </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <MainApp />
+    </BrowserRouter>
+  </StrictMode>,
 )
